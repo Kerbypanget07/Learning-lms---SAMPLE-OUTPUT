d23ff9486f09c2e35f791dd4e99e9e7865d5d7d4
Kerbypanget07 <124670322+Kerbypanget07@users.noreply.github.com>
2023-02-19 20:10:17 +0800

    Add files via upload

---
+++ MY PORTFOLIO SAMPLE OUTPUT.py
@@ -0,0 +1,93 @@
+# Now Loading...
+# Greetings, Welcome to Learning LMS! ver 1.1
+# Would you like to launch the program?
+#
+# Y or N?
+# Response? (Y/N):
+# Now Loading...
+# Choose an option: (1-3)
+#
+#  1: Sign up
+#  2: Login
+#  3: Exit
+#
+# Enter an Option: 1
+#
+# Personal Details: (Sign-In)
+# Provide email address:
+# Your email is Saved
+# Enter a strong password (Should be at least 8 characters):
+# Your password is saved
+#
+# Login to Continue
+#
+# Choose an option: (1-3)
+#
+#  1: Sign up
+#  2: Login
+#  3: Exit
+#
+# Enter an Option: 2
+#
+# Re-Enter your email address: kerby@gmail.com
+# Re-Enter password: aaaaa111
+# Login Credentials Accepted.
+#
+# Welcome to Learning Lms!
+# First in Cyberlms in the whole country!
+# Date and time as of right now: 2023-02-19 20:03:41.387806
+#
+# 1: Lesson Materials
+# 2: Personal Information
+# 3: Calculator
+# 4: Exit
+#
+# Please choose any action (1-4): 1
+# 1 is loading...
+# 1: Lesson Materials
+# 2: Personal Information
+# 3: Calculator
+# 4: Exit
+#
+# Please choose any action (1-4): 2
+# 2 is loading...
+#
+# Please Login in order for your credentials to be saved:
+#
+# Please enter your Full name: Jan kerby P. Maddagan
+# Please enter your Current age: 18
+# Please enter your Full address: Ibabang Dupay, Lucena City
+#
+# Name:
+# Age:
+# Address:
+# Saving...
+#
+# Login Credentials Saved!
+#
+# 1: Lesson Materials
+# 2: Personal Information
+# 3: Calculator
+# 4: Exit
+#
+# Please choose any action (1-4): 3
+# 3 is loading...
+#
+# Welcome to Calculator 1.1!
+#
+# Please enter the first Digit:
+# Please type in the second Digit:
+# Please choose an operation sign (+, -, *, /): *
+#
+# Calculating...
+#  is the answer.
+#
+# 1: Lesson Materials
+# 2: Personal Information
+# 3: Calculator
+# 4: Exit
+#
+# Please choose any action (1-4): 4
+# Goodbye.
+#
+# Process finished with exit code 0
